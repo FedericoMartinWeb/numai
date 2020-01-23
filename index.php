f4a355e2fbd9b19e1e34b4923287a3879e74123d
FedericoMartinWeb <federicompalacios@hotmail.com>
2020-01-23 00:24:11 -0300

    avances

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <?php include 'includes/header.php';?>
 
-	<div class="hero">
+	<section class="hero">
 
 		<div class="hero__image">
 			<img src="img/hero/hero.jpg" alt="">
@@ -13,7 +13,7 @@
 			<p>asdasd</p>
 		</div>
 
-	</div>
+	</section>
 
 	<div class="title">
 		<div class="title__line"></div>
@@ -33,5 +33,107 @@
 			</div>
 		</div>
 	</div>
+
+	<section class="box separador">
+		<div class="container">
+			<div class="row">
+				<div class="box__item col-md-4">
+					<div class="box__internal">
+						<div class="box__title">
+							<h3>Parques</h3>
+						</div>
+
+						<div class="box__description">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic totam illum nesciunt vel quod necessitatibus cum. Eos quo, exercitationem voluptatem quos autem assumenda architecto laudantium debitis iure ipsam facere unde?</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="box__item col-md-4">
+					<div class="box__internal">
+						<div class="box__title">
+							<h3>Ofertas Culturales</h3>
+						</div>
+
+						<div class="box__description">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic totam illum nesciunt vel quod necessitatibus cum. Eos quo, exercitationem voluptatem quos autem assumenda architecto laudantium debitis iure ipsam facere unde?</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="box__item col-md-4">
+					<div class="box__internal">
+						<div class="box__title">
+							<h3>Accesibilidad</h3>
+						</div>
+
+						<div class="box__description">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic totam illum nesciunt vel quod necessitatibus cum. Eos quo, exercitationem voluptatem quos autem assumenda architecto laudantium debitis iure ipsam facere unde?</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="map">
+		<div class="map__wrapper">
+			<img src="img/map/map.jpg" alt="">
+		</div>
+	</section>
+
+	<section class="proyecto separador numai">
+		<div class="title">
+			<div class="title__line"></div>
+			<div class="container">
+				<div class="title__wrapper">
+					<div class="row">
+
+						<div class="title__main col-md-4">
+							<h2>Ubicacion</h2>
+						</div>
+
+						<div class="title__description col-md-8">
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa dignissimos placeat minus dolorem, obcaecati quia animi. Dicta perspiciatis beatae est nesciunt impedit expedita optio maxime? Nobis fugiat et eos facere.
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			
+			<div class="proyecto__item row odd">
+				<div class="proyecto__foto col-md-6">
+					<figure>
+						<img src="img/proyectos/imagen1.jpg" alt="">
+					</figure>
+				</div>
+				<div class="proyecto__texto col-md-4">
+					<div class="proyecto__texto__wrapper">
+						<h4>Donde Todo Comienza</h4>
+						<p>Numai te ofrece una combinacion</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="proyecto__item row even">
+				<div class="proyecto__texto col-md-4">
+					<div class="proyecto__texto__wrapper">
+						<h4>Donde Todo Comienza</h4>
+						<p>Numai te ofrece una combinacion</p>
+					</div>
+				</div>
+
+				<div class="proyecto__foto col-md-8">
+					<figure>
+						<img src="img/proyectos/imagen2.jpg" alt="">
+					</figure>
+				</div>
+			</div>
+
+		</div>
+	</section>
 
 <?php include 'includes/footer.php';?>
