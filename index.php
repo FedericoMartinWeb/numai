998f13f5fb0b267a377de2d17347a801bf75ba0d
FedericoMartinWeb <federicompalacios@hotmail.com>
2020-02-29 19:04:02 -0300

    google map

--- index.php
+++ index.php
@@ -90,11 +90,32 @@
 		</div>
 	</section>
 
-	<section class="map">
+	<section class="map" id="map">
 		<div class="map__wrapper">
 			<img src="img/map/map.png" alt="">
 		</div>
 	</section>
+
+	<script>
+	// Initialize and add the map
+	function initMap() {
+	// The location of Uluru
+	var uluru = {lat: -34.600065, lng: -58.429722};
+	// The map, centered at Uluru
+	var map = new google.maps.Map(
+		document.getElementById('map'), {zoom: 14, center: uluru});
+	// The marker, positioned at Uluru
+	var marker = new google.maps.Marker({position: uluru, map: map});
+	}
+		</script>
+		<!--Load the API from the specified URL
+		* The async attribute allows the browser to render the page while the API loads
+		* The key parameter will contain your own API key (which is not needed for this tutorial)
+		* The callback parameter executes the initMap() function
+		-->
+		<script async defer
+		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAPK9hiFigksADUMV76aiQK1swczDyyohM&callback=initMap">
+		</script>
 
 	<section class="proyecto separador numai" id="proyecto">
 		<div class="title">
@@ -637,7 +658,7 @@
 					</div>
 
 					<div class="map__img">
-					<a href="img/planos/planos-1.jpg" data-lightbox="planm">
+					<a href="img/planos/planos-1.jpg" data-lightbox="pland">
 						<img src="img/planos/planos-1.jpg" alt="">
 					</a>
 					</div>
